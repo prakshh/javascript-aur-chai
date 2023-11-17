15266db72786c860697d38bd644e0cf6ebb790ec
Prakash Das <prakashdas18@gmail.com>
2023-11-17 06:27:13 +0000

    typeof null and undefined

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,12 +1,38 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node.js, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+/*
+js official documentation
+
+https://tc39.es/ecma262/
+
+https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+
+*/
+
+console.log("Prakshh")
+
+let name = "Prakshh"
+let age = 18
+let isLoggedIn = false
+
 
 /*
 number
-bigInt 
-string
-boolean
-null
+bigint 
+string => ""
+boolean => true/false
+null => standalone value
 undefined
-symbol
+symbol => unique
 
 object
 */
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
