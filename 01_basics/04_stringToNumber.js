edd12df295c5ea78cdd525d7063323218aa60e03
prakshh <prakashdas18@gmail.com>
2024-03-01 23:58:23 +0530

    stringToNumber

---
+++ 01_basics/04_stringToNumber.js
@@ -0,0 +1,21 @@
+
+let value = 3
+let negValue = -value
+console.log(negValue)   //-3
+
+let str1 = 'Hello'
+let str2 = ' Tony'
+let str3 = str1 + str2 
+console.log(str3)   //Hello Tony
+
+console.log("1" + 2)    //12
+console.log(1 + "2")    //12
+console.log("1" + 2 + 2)    //122
+console.log(1 + 2 + "2")    //32
+
+//https://tc39.es/
+//https://tc39.es/ecma262/multipage/abstract-operations.html
+
+console.log(true)   //true
+console.log(+true)  //1
+//console.log(true+)  //SyntaxError: Unexpected token ')'
